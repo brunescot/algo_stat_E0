3569648022f11e281af489130f09c4f85d973c54
Bruno <brunescot.reis@gmail.com>
2026-03-02 15:54:58 -0300

    asdda

--- E0_stat.py
+++ E0_stat.py
@@ -1,10 +1,12 @@
 import numpy as np
-
+g = 9.7864
 in_i = 0.05
 
 med_br = [15.0, 16.9, 10.9, 14.0, 14.8]
 med_fe = [18.1, 20.3, 8.6, 8.9, 6.5]
 med_pe = [12.9, 14.4, 13.3, 13.9, 11.5]
+
+t_bruno = t_felps = t_pedrao = []
 
 def variancia (lista) :
     avg = np.mean(lista)
@@ -22,28 +24,53 @@ def desvio_medio (lista) :
 def desvio_c (lista) :
     return round(np.sqrt(in_i**2 + desvio_medio(lista)**2), 4)
 
-def medir (lista) :
+
+def tempo_grav (h):
+    return np.sqrt(2*h/(100*g))
+
+def tempo (lista) :
+    t = []
+    for h in lista:
+        t.append(round(tempo_grav(h), 4))
+    return np.array(t)
+
+
+
+def medir(lista):
     avg = round(np.mean(lista), 4)
-    d_m = desvio_medidas(lista)
-    d_mm = desvio_medio(lista)
-    d_c = desvio_c(lista)
+    d_c = round(desvio_c(lista), 4)
+    print("valor medio = ", round(np.mean(lista), 4))
+    print("desvio_medidas = ", round(desvio_medidas(lista), 4))
+    print("desvio_medio =", round(desvio_medio(lista), 4))
+    print("desvio_c =", d_c)
+    print("v_medio + inc = ", avg, "+-",d_c)
 
-    print("t_medio = ", avg)
-    print("desvio_medidas = ", d_m)
-    print("desvio_medio =", d_mm)
-    print("desvio_c =",d_c)
-    print("t_medio + inc = ", avg, "+-", d_c)
 
-print("bruno: ")
+
+
+
+print("y bruno: ")
 medir(med_br)
 print("\n")
-
-print("felps: ")
-medir(med_fe)
+print("tempo bruno:")
+print(tempo(med_br))
+medir(tempo(med_br))
 print("\n")
 
-print("pedrao: ")
+print("y felps: ")
+medir(med_fe)
+print("\n")
+print("tempo felps: ")
+print(tempo(med_fe))
+medir(tempo(med_fe))
+print("\n")
+
+print("y pedrao: ")
 medir(med_pe)
+print("\n")
+print("tempo pedrao: ")
+print(tempo(med_pe))
+medir(tempo(med_pe))
 
 
 
